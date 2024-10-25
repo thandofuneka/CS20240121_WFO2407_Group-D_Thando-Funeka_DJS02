c305915043d76890ed49a7c56fd7dbb870d5a0d8
Thando <thandofuneka1@.com>
2024-10-25 17:54:44 +0200

    Done

--- scripts.js
+++ scripts.js
@@ -5,5 +5,27 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+  
+
+  //Check if inputs are provided
+  if(!dividend || !divider) {
+    result.innerText = "Please enter both values!";
+    return;
+  }
+
+  //Convert to numbers and check if the divider is 0
+  const dividedNum = parseFloat(dividend);
+  const dividerNum = parseFloat(divider);
+
+  if(isNaN(dividedNum) || isNaN(dividerNum)) {
+    result.innerText = "Please enter valid numbers!";
+    return;
+  }
+
+  if(dividerNum === 0) {
+    result.innerText = "Division can't occur with 0 as the divider!!!!";
+    return;
+  }
+
+result.innerText = dividedNum / dividerNum;
 });
